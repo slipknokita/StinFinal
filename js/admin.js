cefa31c3b6396070648d7de8e5785eb79099b7b6
Ivo Montanari <yivodark@gmail.com>
2020-10-16 14:18:36 -0300

    Correccion de muchos detalles menores

--- js/admin.js
+++ js/admin.js
@@ -80,4 +80,31 @@ const form = document.getElementById('form');
  }
 
 
- getTodos().then(Games => buildTodo(Games));
+getTodos().then(Games => buildTodo(Games));
+
+let loginActive = document.getElementById("responsiveUser");
+function loginOn (){
+    if (localStorage.getItem("userActive") == "admin"){
+        loginActive.innerHTML=`
+            <a class="nav-link dropdown-toggle active text-right" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Bienvenido ${localStorage.getItem("userActive")}</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="admin.html">Panel de Administracion</a>
+              <button class="dropdown-item" onclick="removeLS()">Salir</button>
+            </div>
+            `
+    } else if (localStorage.getItem("userActive") != null){
+        loginActive.innerHTML=`
+          <a class="nav-link dropdown-toggle active text-right" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Bienvenido ${localStorage.getItem("userActive")}</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <button class="dropdown-item" onclick="removeLS()">Salir</button>
+          </div>
+          `
+      }
+}
+loginOn();
+function removeLS(){
+  localStorage.removeItem("userActive");
+  location.reload();
+}
--- js/carousel3.js
+++ js/carousel3.js
@@ -263,7 +263,34 @@ const getData = url_api => {
     console.log(error);
   })*/
 
- 
+ // Ivo
+
+let loginActive = document.getElementById("responsiveUser");
+function loginOn (){
+    if (localStorage.getItem("userActive") == "admin"){
+        loginActive.innerHTML=`
+            <a class="nav-link dropdown-toggle active text-right" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Bienvenido ${localStorage.getItem("userActive")}</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="admin.html">Panel de Administracion</a>
+              <button class="dropdown-item" onclick="removeLS()">Salir</button>
+            </div>
+            `
+    } else if (localStorage.getItem("userActive") != null){
+        loginActive.innerHTML=`
+          <a class="nav-link dropdown-toggle active text-right" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Bienvenido ${localStorage.getItem("userActive")}</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <button class="dropdown-item" onclick="removeLS()">Salir</button>
+          </div>
+          `
+      }
+}
+loginOn();
+function removeLS(){
+  localStorage.removeItem("userActive");
+  location.reload();
+}
 
 
  function removeAll() {
